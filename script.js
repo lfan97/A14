567b5f94534b9017b0bbb7214e5ad8230f787f31
lfan97 <lf97@njit.edu>
2020-07-01 11:30:04 -0400

    A14 commit second

--- script.js
+++
@@ -1,8 +0,0 @@
-Cookies.set('cookieCookie', 'true', {expires:365});
-
-var myCookie = Cookies.get('cookieCookie');
-
-var title = document.querySelect('h1')
-if(myCookie){
-    title.classList.add('cookie')
-}
